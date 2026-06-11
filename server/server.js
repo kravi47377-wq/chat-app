76c9de81b5684308d6e68c64d3504eec14a4debd
kravi47377-wq <kravi47377@gmail.com>
2026-06-11 13:50:00 +0530

    mongodb connected

--- server/server.js
+++ server/server.js
@@ -2,38 +2,78 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const MessageSchema = new mongoose.Schema({
+sender: String,
+text: String,
+createdAt: {
+type: Date,
+default: Date.now
+}
+});
+
+const Message = mongoose.model("Message", MessageSchema);
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  },
+cors: {
+origin: "*",
+methods: ["GET", "POST"]
+}
 });
 
-io.on("connection", (socket) => {
-  console.log("User Connected");
+io.on("connection", async (socket) => {
 
-  socket.on("send_message", (data) => {
-    io.emit("receive_message", data);
-  });
+console.log("User Connected");
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected");
-  });
+const history =
+await Message.find().sort({ createdAt: 1 });
+
+socket.emit("chat_history", history);
+
+socket.on("send_message", async (data) => {
+
+await Message.create({
+sender: data.sender,
+text: data.text
+});
+
+io.emit("receive_message", data);
+
+});
+
+socket.on("disconnect", () => {
+console.log("User Disconnected");
+});
+
 });
 
 app.get("/", (req, res) => {
-  res.send("Duggu Server Running");
+res.send("Duggu Server Running");
+});
+
+app.get("/messages", async (req, res) => {
+
+const messages =
+await Message.find().sort({ createdAt: 1 });
+
+res.json(messages);
+
 });
 
 const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
-  console.log(`Server Running on Port ${PORT}`);
+console.log(`Server Running on Port ${PORT}`);
 });
